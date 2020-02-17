91f55d27f33d0030ce33ae1726763977789e6771
Mwehrhei <59847516+Mwehrhei@users.noreply.github.com>
2020-02-17 16:09:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+var tweets = [
+   {
+     text:"Doggo ipsum very jealous pupper extremely cuuuuuute yapper length boy h*ck long woofer pats extremely cuuuuuute, much ruin diet pupper floofs corgo long bois extremely cuuuuuute. porgo length boy puggo. You are doing me a frighten thicc puggo big ol what a nice floof, yapper porgo heck. Doing me a frighten borkdrive waggy wags, tungg. Shoober vvv smol ruff, shoob. Ruff wrinkler pupperino length boy heckin good boys fat boi shoober stop it fren heck, corgo long woofer yapper woofer borkf I am bekom fat long doggo. Pats very good spot floofs waggy wags, snoot. Ur givin me a spook long doggo doggo, wrinkler. He made many woofs heck blep long water shoob, shoober. Heckin good boys extremely cuuuuuute fat boi shibe, floofs heckin good boys and girls pupper, aqua doggo big ol pupper. boof ur givin me a spook. Blop wrinkler puggo big ol pupper shooberino doggorino, you are doing me a frighten woofer waggy wags.  Woofer waggy wags boofers corgo smol, clouds heckin good boys. Pupper mlem puggo, heckin angery woofer. Borkdrive long water shoob tungg long doggo he made many woofs fat boi, ur givin me a spook doggorino floofs thicc floofs, much ruin diet borking doggo long doggo very hand that feed shibe. H*ck shooberino corgo long woofer h*ck most angery pupper I have ever seen lotsa pats, fat boi doge what a nice floof pats long water shoob. Wrinkler fat boi h*ck pats, woofer yapper.",
+     likes:50,
+     comments:100000,
+     retweet:20,
+     image:true,
+     username:"snuffleluffugus",
+   },
+    {
+     text:"Porgo length boy puggo. You are doing me a frighten thicc puggo big ol what a nice floof, yapper porgo heck. Doing me a frighten borkdrive waggy wags, tungg. Shoober vvv smol ruff, shoob. Ruff wrinkler pupperino length boy heckin good boys fat boi shoober stop it fren heck, corgo long woofer yapper woofer borkf I am bekom fat long doggo. Pats very good spot floofs waggy wags, snoot. Ur givin me a spook long doggo doggo, wrinkler. He made many woofs heck blep long water shoob, shoober. Heckin good boys extremely cuuuuuute fat boi shibe, floofs heckin good boys and girls pupper, aqua doggo big ol pupper. boof ur givin me a spook. Blop wrinkler puggo big ol pupper shooberino doggorino, you are doing me a frighten woofer waggy wags.  Woofer waggy wags boofers corgo smol, clouds heckin good boys. Pupper mlem puggo, heckin angery woofer. Borkdrive long water shoob tungg long doggo he made many woofs fat boi, ur givin me a spook doggorino floofs thicc floofs, much ruin diet borking doggo long doggo very hand that feed shibe. H*ck shooberino corgo long woofer h*ck most angery pupper I have ever seen lotsa pats, fat boi doge what a nice floof pats long water shoob. Wrinkler fat boi h*ck pats, woofer yapper.",
+     likes:20,
+     comments:1000,
+     retweet:20000,
+     image:true,
+     username:"Goku"
+   },
+   {
+     text:"Porgo length boy puggo. You are doing me a frighten thicc puggo big ol what a nice floof, yapper porgo heck. Doing me a frighten borkdrive waggy wags, tungg. Shoober vvv smol ruff, shoob. Ruff wrinkler pupperino length boy heckin good boys fat boi shoober stop it fren heck, corgo long woofer yapper woofer borkf I am bekom fat long doggo. Pats very good spot floofs waggy wags, snoot. Ur givin me a spook long doggo doggo, wrinkler. He made many woofs heck blep long water shoob, shoober. Heckin good boys extremely cuuuuuute fat boi shibe, floofs heckin good boys and girls pupper, aqua doggo big ol pupper. boof ur givin me a spook. Blop wrinkler puggo big ol pupper shooberino doggorino, you are doing me a frighten woofer waggy wags.  Woofer waggy wags boofers corgo smol, clouds heckin good boys. Pupper mlem puggo, heckin angery woofer. Borkdrive long water shoob tungg long doggo he made many woofs fat boi, ur givin me a spook doggorino floofs thicc floofs, much ruin diet borking doggo long doggo very hand that feed shibe. H*ck shooberino corgo long woofer h*ck most angery pupper I have ever seen lotsa pats, fat boi doge what a nice floof pats long water shoob. Wrinkler fat boi h*ck pats, woofer yapper.",
+     likes:2000,
+     comments:51000,
+     retweet:1000,
+     image:true,
+     username:"Shrek"
+   },
+{
+     text:"Porgo length boy puggo. You are doing me a frighten thicc puggo big ol what a nice floof, yapper porgo heck. Doing me a frighten borkdrive waggy wags, tungg. Shoober vvv smol ruff, shoob. Ruff wrinkler pupperino length boy heckin good boys fat boi shoober stop it fren heck, corgo long woofer yapper woofer borkf I am bekom fat long doggo. Pats very good spot floofs waggy wags, snoot. Ur givin me a spook long doggo doggo, wrinkler. He made many woofs heck blep long water shoob, shoober. Heckin good boys extremely cuuuuuute fat boi shibe, floofs heckin good boys and girls pupper, aqua doggo big ol pupper. boof ur givin me a spook. Blop wrinkler puggo big ol pupper shooberino doggorino, you are doing me a frighten woofer waggy wags.  Woofer waggy wags boofers corgo smol, clouds heckin good boys. Pupper mlem puggo, heckin angery woofer. Borkdrive long water shoob tungg long doggo he made many woofs fat boi, ur givin me a spook doggorino floofs thicc floofs, much ruin diet borking doggo long doggo very hand that feed shibe. H*ck shooberino corgo long woofer h*ck most angery pupper I have ever seen lotsa pats, fat boi doge what a nice floof pats long water shoob. Wrinkler fat boi h*ck pats, woofer yapper.",
+     likes:2020,
+     comments:10300,
+     retweet:240000,
+     image:true,
+     username:"Naruto"
+   },
+{
+     text:"Porgo length boy puggo. You are doing me a frighten thicc puggo big ol what a nice floof, yapper porgo heck. Doing me a frighten borkdrive waggy wags, tungg. Shoober vvv smol ruff, shoob. Ruff wrinkler pupperino length boy heckin good boys fat boi shoober stop it fren heck, corgo long woofer yapper woofer borkf I am bekom fat long doggo. Pats very good spot floofs waggy wags, snoot. Ur givin me a spook long doggo doggo, wrinkler. He made many woofs heck blep long water shoob, shoober. Heckin good boys extremely cuuuuuute fat boi shibe, floofs heckin good boys and girls pupper, aqua doggo big ol pupper. boof ur givin me a spook. Blop wrinkler puggo big ol pupper shooberino doggorino, you are doing me a frighten woofer waggy wags.  Woofer waggy wags boofers corgo smol, clouds heckin good boys. Pupper mlem puggo, heckin angery woofer. Borkdrive long water shoob tungg long doggo he made many woofs fat boi, ur givin me a spook doggorino floofs thicc floofs, much ruin diet borking doggo long doggo very hand that feed shibe. H*ck shooberino corgo long woofer h*ck most angery pupper I have ever seen lotsa pats, fat boi doge what a nice floof pats long water shoob. Wrinkler fat boi h*ck pats, woofer yapper.",
+     likes:2064,
+     comments:911000,
+     retweet:27000,
+     image:true,
+     username:"Donatello"
+   },
+ ]
+
+ var tweetEles=[];
+ 
+ function tweet(twt){
+   var that=this;
+   
+   this.retweet=twt.retweet;
+   this.comments=twt.comments;
+   this.likes=twt.likes;
+   
+   this.ele=document.createElement("div");
+   this.ele.classList.add("tweet");
+   this.usernameEle=document.createElement("div");
+   this.textEle=document.createElement("div");
+   this.retweetEle=document.createElement("div");
+   this.commentsEle=document.createElement("div");
+   this.imageEle=document.createElement("div");
+   this.likesEle=document.createElement("div");
+   
+   this.usernameEle.innerHTML=twt.username;
+   this.textEle.innerHTML=twt.text;
+   this.retweetEle.innerHTML="Retweets:"+twt.retweet;
+   this.commentsEle.innerHTML="Comments:"+twt.comments;
+   this.likesEle.innerHTML="Likes:"+twt.likes;
+   
+   this.imageEle.innerHTML="<img src='https://media.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.gif'/>"
+   
+   this.retweetEle.addEventListener("click", function(){
+     that.retweet=that.retweet+1;
+     that.retweetEle.innerHTML="Retweets: "+that.retweet;
+     })
+   this.commentsEle.addEventListener("click", function(){
+     that.comments=that.comments+1;
+     that.commentsEle.innerHTML="Comments: "+that.comments;
+     })
+   this.likesEle.addEventListener("click", function(){
+     that.likes=that.likes+1;
+     that.likesEle.innerHTML="Likes: "+that.likes;
+     if(that.likes>=100){
+        that.ele.style.backgroundColor="red";
+        that.ele.style.color="white";
+     }
+     })
+   
+   
+   this.ele.appendChild(this.usernameEle);
+   this.ele.appendChild(this.textEle);
+   this.ele.appendChild(this.retweetEle);
+   this.ele.appendChild(this.commentsEle);
+   if(twt.image){
+      this.ele.appendChild(this.imageEle);
+      }
+   this.ele.appendChild(this.imageEle);
+   this.ele.appendChild(this.likesEle);
+   
+   document.body.appendChild(this.ele);
+ }
+
+for(var i=0; i<tweets.length; i++){
+  tweetEles.push(new tweet(tweets[i]))
+  
+}
+
+
+
+
+
+
+//likes retweets comments as numbers
+//if image, generate gif
+//100 likes = color of the like
